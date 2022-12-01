94ff48b844e756dc1ea3037383da6ee50a031357
Waqar Khan <sms.raees@gmail.com>
2022-12-02 00:43:00 +0500

    added comments in url

--- academics/urls.py
+++ academics/urls.py
@@ -4,9 +4,12 @@ from . import views as academics_views
 app_name = "academics"
 
 urlpatterns = [
+    # SECTION URLS
     path("sections/", academics_views.create_section, name="create_section"),
     path("section/<int:pk>/update/", academics_views.update_section, name="update_section"),
     path("section/<int:pk>/delete/", academics_views.delete_section, name="delete_section"),
+    
+    # CLASS URLS
     path("class/", academics_views.create_class, name="create_class"),
     path("class/<int:pk>/update/", academics_views.update_class, name="update_class"),
     path("class/<int:pk>/delete/", academics_views.delete_class, name="delete_class"),
